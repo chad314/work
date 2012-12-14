88ae465e2ccc1f94cfc183895c48b060e48918fd
root <root@limepi.(none)>
2012-12-14 21:58:01 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Expert::Application.config.secret_token = '4e386c46478f60a8293a7e58bc227b25da844a3227bfeac950685cd227f0ba4b8773c2322a5fae56b75f39ba2be7d8e9995c2eff1e97484ebcd2599024e4fbf9'
